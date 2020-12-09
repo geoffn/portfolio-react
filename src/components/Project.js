7565cf01405d9ee33bb376ca192970d0363a4f9c
Geoff Nelson <geoffreysnelson@gmail.com>
2020-12-09 12:42:16 -0800

    rc 1

--- src/components/Project.js
+++ src/components/Project.js
@@ -51,7 +51,10 @@ export default function Project() {
                                 </span>
                                 <p className="my-6 text-lg text-gray-700 leading-relaxed">{project.description}
                                 </p>
-                                <span></span>
+                                <a href={project.link} rel="noopener noreferrer" target="_blank" className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl">
+                                    View the Project {" "}
+                                    <span role="img" aria-label="right pointer">👉</span>
+                                </a>
                             </div>
                         </article>
                     )}
